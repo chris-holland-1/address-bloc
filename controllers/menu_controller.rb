440d099a4580da0572f375f931cb60eda298189b
Chris Holland <chris.holland987@gmail.com>
2017-02-10 11:42:17 -0800

    Assignment 6 Menu

--- controllers/menu_controller.rb
+++ controllers/menu_controller.rb
@@ -10,10 +10,11 @@ class MenuController
   def main_menu
     puts "main Menu - #{address_book.entries.count} entries"
     puts "1 - View all entries"
-    puts "2 - Create an entry"
-    puts "3 - Search for an entry"
-    puts "4 - Import entries from a CSV"
-    puts "5 - Exit"
+    puts "2 - View Entry Number n"
+    puts "3 - Create an entry"
+    puts "4 - Search for an entry"
+    puts "5 - Import entries from a CSV"
+    puts "6 - Exit"
     print "Enter your selection:"
 
     selection = gets.to_i
@@ -25,17 +26,21 @@ class MenuController
         main_menu
       when 2
         system "clear"
-        create_entry
+        view_n_entry
         main_menu
       when 3
         system "clear"
-        search_entries
+        create_entry
         main_menu
       when 4
         system "clear"
-        read_csv
+        search_entries
         main_menu
       when 5
+        system "clear"
+        read_csv
+        main_menu
+      when 6
         puts "Good-bye!"
         exit(0)
       else
@@ -54,6 +59,23 @@ class MenuController
 
     system "clear"
     puts "End of entries"
+  end
+
+  def view_n_entry
+    print "What is the entry?"
+    selection = gets.chomp.to_i
+
+      if selection < @address_book.entries.count
+        puts @address_book.entries[selection]
+        gets.chomp
+        system "clear"
+      else
+        puts "Please enter a valid entry"
+        view_n_entry
+      end
+
+      system "clear"
+      puts "End of entry"
   end
 
   def create_entry
